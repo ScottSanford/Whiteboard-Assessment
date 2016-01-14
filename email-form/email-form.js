48a440b4a6651e7a9532ca8207b8b6e191cd5e11
Scott Sanford <ssanford@mediafly.com>
2016-01-14 14:51:10 -0600

    Email Validation is complete

--- email-form/email-form.js
+++ email-form/email-form.js
@@ -2,21 +2,33 @@ angular.module('assessmentApp')
 
 .controller('EmailFormCtrl', function($scope, $location, localStorageService){
 
-	$scope.currentIndex = 5;
-	$scope.barValue = 6;
+	$scope.currentIndex      = 5;
+	$scope.barValue          = 6;
 	$scope.totalNumQuestions = 6;
+	$scope.firstNameReq      = false;
+	$scope.emailReq          = false;
 
 	var finalScore = localStorageService.get('score');
 
 	$scope.submitForm = function() {
-		if ($scope.resultsForm.$valid) {
-			console.log($scope.resultsForm);
-		}
+
+		var firstNameValid = $scope.resultsForm.firstName.$valid;
+		var emailValid     = $scope.resultsForm.userEmail.$valid;
+
+		var validName = !firstNameValid ? true : false;
+		$scope.firstNameReq = validName; 
+
+		var validEmail = !emailValid ? true : false;
+		$scope.emailReq = validEmail;
 		// send results to HubSpot API 
 
+		if (firstNameValid && emailValid) {
+
+			$location.url('/results');
+			
+		}
 
 		// take user to Results page 
-		$location.url('/results');
 
 	}
 
